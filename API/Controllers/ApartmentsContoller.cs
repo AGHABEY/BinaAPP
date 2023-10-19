9236288b57cefb629abaa58a2061ead470964610
AGHABEY <rafet.agabeyli98@gmail.com>
2023-10-19 13:36:36 +0400

    Interface Data added

--- API/Controllers/ApartmentsContoller.cs
+++ API/Controllers/ApartmentsContoller.cs
@@ -6,30 +6,27 @@ using Domain;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Persistence;
-using MediatR;
-using System.Runtime.CompilerServices;
-using Application.Apartments;
 
 namespace API.Controllers
 {
     public class ApartmentsContoller : BaseApiController
     {
-        private readonly Mediator _mediator;
-       public ApartmentsContoller(Mediator mediator)
+        private readonly DataContext _context;
+       public ApartmentsContoller(DataContext context)
        {
-          _mediator=mediator;
+          _context=context;
        }   
 
        [HttpGet] //api/apartments
        public async Task<ActionResult<List<Apartment>>> GetApartments()
        {
-         return await _mediator.Send(new List.Query());
+         return await _context.Apartments.ToListAsync();
        }
 
        [HttpGet("{id}")]
        public async Task<ActionResult<Apartment>> GetApartment(Guid id)
        {
-          return Ok();
+          return await _context.Apartments.FindAsync(id);
        }
     }
 
--- client-app/src/App.tsx
+++ client-app/src/App.tsx
@@ -1,8 +1,31 @@
+import { useEffect,useState } from 'react'
 import './App.css'
+import axios from 'axios'
+
 
 function App() {
+  const [apartments, setApartments]=useState([]);
+
+  useEffect(()=>{
+       axios.get('http://localhost:5000/api/ApartmentsContoller')
+       .then(
+        response=>{
+          setApartments(response.data)
+          console.log(response.data)
+        }
+       )
+  })
   return(
+    <div>
     <h1>Bina APP</h1>
+    <ul>
+      {apartments.map((apartment:any) =>(
+        <li key={apartment.id}>
+          {apartment.ownerName}
+       </li>
+      ))}
+    </ul>
+    </div>
   )
 }
 
