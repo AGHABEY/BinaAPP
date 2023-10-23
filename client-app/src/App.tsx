9bb592b2b147ec30658ea8fc06fdb8f4c4b274fd
AGHABEY <rafet.agabeyli98@gmail.com>
2023-10-24 00:06:46 +0400

    Add Semantic UI and table

--- client-app/src/App.tsx
+++ client-app/src/App.tsx
@@ -1,6 +1,7 @@
 import { useEffect,useState } from 'react'
 import './App.css'
 import axios from 'axios'
+import { Button, Header, Table } from 'semantic-ui-react';
 
 
 function App() {
@@ -11,20 +12,43 @@ function App() {
        .then(
         response=>{
           setApartments(response.data)
-          console.log(response.data)
         }
        )
   })
   return(
     <div>
-    <h1>Bina APP</h1>
-    <ul>
-      {apartments.map((apartment:any) =>(
-        <li key={apartment.id}>
-          {apartment.ownerName}
-       </li>
+    <Header as='h2' icon='users' content='Apartments'/>
+    
+     
+        <Table celled>
+            <Table.Header>
+              <Table.Row>
+                <Table.HeaderCell>Adi</Table.HeaderCell>
+                <Table.HeaderCell>Soyadi</Table.HeaderCell>
+                <Table.HeaderCell>Telefon Nomresi</Table.HeaderCell>
+                <Table.HeaderCell>Menzilin NO</Table.HeaderCell>
+                <Table.HeaderCell>Mertebe</Table.HeaderCell>
+                <Table.HeaderCell>Blok</Table.HeaderCell>
+              </Table.Row>
+            </Table.Header>
+            {apartments.map((apartment:any) =>(
+            <Table.Body>
+              <Table.Row>
+                <Table.Cell> {apartment.ownerName}</Table.Cell>
+                <Table.Cell> {apartment.ownerSurname}</Table.Cell>
+                <Table.Cell>{apartment.ownerPhoneNumber}</Table.Cell>
+                <Table.Cell>{apartment.apartmentNumber}</Table.Cell>
+                <Table.Cell>{apartment.floor}</Table.Cell>
+                <Table.Cell>{apartment.block}</Table.Cell>
+                <Table.Cell> <Button content='Info' primary /></Table.Cell>
+                <Table.Cell> <Button content='Edit' secondary /></Table.Cell>
+              </Table.Row>
+            </Table.Body>
+         
       ))}
-    </ul>
+       </Table>
+    
+
     </div>
   )
 }
